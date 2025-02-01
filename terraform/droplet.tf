27962a2e328c3aa28c9ddfa54b7a6701cf7acd1f
FlorencioFer <fur_oliveira@hotmail.com>
2025-02-01 14:21:08 -0300

    creating droplet to host Nats

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -1,8 +1,23 @@
-resource "digitalocean_droplet" "my_video" {
+resource "digitalocean_droplet" "my_video_nats_server" {
   image    = "docker-20-04"
-  name     = "my-burger-api-gtw"
+  name     = "my-video-nats-server"
   region   = "nyc1"
   size     = "s-1vcpu-1gb"
   vpc_uuid = data.digitalocean_vpc.vpc.id
   ssh_keys = [43452957, 43405929]
+}
+
+resource "null_resource" "install_nats" {
+  connection {
+    type        = "ssh"
+    host        = digitalocean_droplet.my_video_nats_server.ipv4_address
+    user        = "root"
+    private_key = file(var.ssh_private_key)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker run -d --name nats-server -p 4222:4222 -p 8222:8222 nats:latest"
+    ]
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
 variable "do_token" {
   type = string
 }
+
+variable "ssh_private_key" {
+  description = "Conteúdo da chave privada SSH"
+  type        = string
+  sensitive   = true
+}
