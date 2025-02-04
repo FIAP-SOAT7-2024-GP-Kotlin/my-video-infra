d33affc68a5023b70a1b2b90ee8342ca64db77e2
FlorencioFer <fur_oliveira@hotmail.com>
2025-02-03 21:50:43 -0300

    test ssh

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -11,5 +11,10 @@ resource "digitalocean_droplet" "my_video_nats_server" {
   user_data = <<-EOF
              #!/bin/bash
              docker run -d --name nats -p 4222:4222 -p 8222:8222 -p 6222:6222 nats:latest
+             sleep 10
+             docker exec nats nats -s nats://localhost:4222 pub video.pending "Creating video.pending"
+             docker exec nats nats -s nats://localhost:4222 pub video.processing "Creating video.processing"
+             docker exec nats nats -s nats://localhost:4222 pub video.completed "Creating video.completed"
+             docker exec nats nats -s nats://localhost:4222 pub video.error "Creating video.error"
              EOF
 }
