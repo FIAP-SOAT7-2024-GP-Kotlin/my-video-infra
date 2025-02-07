321a204134606974def3e941f573417f8354744d
FlorencioFer <fur_oliveira@hotmail.com>
2025-02-06 21:28:18 -0300

    creating nats

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "my_video_kubernetes_cluster" {
   name     = "my-video-k8s"
   region   = "nyc1"
-  version  = "1.31.1-do.5"
+  version  = "1.32.1-do.0"
   vpc_uuid = data.digitalocean_vpc.vpc.id
 
   node_pool {
